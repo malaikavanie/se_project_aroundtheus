7b4fc3f9b4ebfc45369647e91a9229638482338f
MALAIKA VANIE <MVANIE@MYYAHOO.COM>
2024-03-16 11:12:33 -0400

    project 9

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -2,9 +2,9 @@ import Popup from "./Popup.js";
 
 export default class PopupWithConfirmation extends Popup {
   //***POPUPWITHCONFIRMATION CONSTRUCTOR***
-  constructor(popupSelector, { submitButtonSelector }, formSelector) {
+  constructor(popupSelector, { submitButtonSelector }) {
     super(popupSelector);
-    this._form = this._popupElement.querySelector(formSelector);
+    this._form = this._popupElement.querySelector(".modal__form");
 
     this._button = this._popupElement.querySelector(submitButtonSelector);
     this._originalButtonText = this._button.textContent;
